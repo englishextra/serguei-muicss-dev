5bef693e38eebaabcd59c4fd11bb940f498abd09
englishextra <englishextra@yandex.ru>
2019-01-15 22:12:12 +0300

    updates

--- libs/serguei-muicss/src/bundle.js
+++ libs/serguei-muicss/src/bundle.js
@@ -2198,11 +2198,11 @@ twttr, unescape, VK, WheelIndicator, Ya*/
 	};
 
 	/* var scripts = [
-			"../../fonts/roboto-fontfacekit/2.137/css/roboto.css",
-			"../../fonts/roboto-mono-fontfacekit/2.0.986/css/roboto-mono.css",
-			"../../cdn/iframe-lightbox/0.2.8/css/iframe-lightbox.fixed.css",
-			"../../cdn/img-lightbox/0.2.1/css/img-lightbox.fixed.css",
-			"../../cdn/mui/0.9.39/css/mui.css"
+				"../../fonts/roboto-fontfacekit/2.137/css/roboto.css",
+				"../../fonts/roboto-mono-fontfacekit/2.0.986/css/roboto-mono.css",
+				"../../cdn/mui/0.9.39/css/mui.css",
+				"../../cdn/iframe-lightbox/0.2.8/css/iframe-lightbox.fixed.css",
+				"../../cdn/img-lightbox/0.2.1/css/img-lightbox.fixed.css"
 	]; */
 
 	var scripts = [
@@ -2255,17 +2255,18 @@ twttr, unescape, VK, WheelIndicator, Ya*/
 	}
 
 	/* var scripts = [
-			"../../cdn/minigrid/3.1.1/js/minigrid.fixed.js",
-			"../../cdn/ReadMore.js/1.0.0/js/readMoreJS.fixed.js",
-			"../../cdn/ripple-js/1.4.4/js/ripple.fixed.js",
-			"../../cdn/iframe-lightbox/0.2.8/js/iframe-lightbox.fixed.js",
-			"../../cdn/img-lightbox/0.2.1/js/img-lightbox.fixed.js",
-			"../../cdn/qrjs2/0.1.7/js/qrjs2.fixed.js",
-			"../../cdn/Tocca.js/2.0.1/js/Tocca.fixed.js",
-			"../../cdn/wheel-indicator/1.1.4/js/wheel-indicator.fixed.js",
-			"../../cdn/resize/1.0.0/js/any-resize-event.fixed.js",
-			"../../cdn/mustache/2.3.0/js/mustache.fixed.js",
-			"../../cdn/EventEmitter/5.2.5/js/EventEmitter.fixed.js"
+				"./bower_components/iframe-lightbox/iframe-lightbox.js",
+				"../../cdn/img-lightbox/0.2.1/js/img-lightbox.fixed.js",
+				"../../cdn/qrjs2/0.1.7/js/qrjs2.fixed.js",
+				"../../cdn/Tocca.js/2.0.1/js/Tocca.fixed.js",
+				"../../cdn/wheel-indicator/1.1.4/js/wheel-indicator.fixed.js",
+				"../../cdn/resize/1.0.0/js/any-resize-event.fixed.js",
+				"./node_modules/macy/dist/macy.js",
+				"../../cdn/mustache/2.3.0/js/mustache.fixed.js",
+				"../../cdn/EventEmitter/5.2.5/js/EventEmitter.fixed.js",,
+				"../../cdn/minigrid/3.1.1/js/minigrid.fixed.js",
+				"../../cdn/ripple-js/1.4.4/js/ripple.fixed.js",
+				"../../cdn/ReadMore.js/1.0.0/js/readMoreJS.fixed.js"
 	]; */
 
 	scripts.push("./libs/serguei-muicss/js/vendors.min.js");
